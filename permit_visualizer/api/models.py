78a8f5c3494d05ca84e7c55ec506dbb90eff628a
Cedric Charly <cedric.charly@gmail.com>
2017-04-12 13:55:11 -0500

    Implement heatmap query

--- permit_visualizer/api/models.py
+++ permit_visualizer/api/models.py
@@ -3,6 +3,7 @@
 
 import bson
 import flask_pymongo
+import structlog
 
 
 mongo = flask_pymongo.PyMongo()
@@ -29,6 +30,47 @@ def all_permits(limit, after=None):
         cursor = mongo.db['all_permits'].find().limit(limit)
 
     permits = [permit for permit in cursor]
+    count = len(permits)
+    if count > 0:
+        pagination_cursor = str(permits[-1]['_id'])
+    else:
+        pagination_cursor = None
+
+    for permit in permits:
+        del permit['_id']
+
+    return permits, pagination_cursor
+
+
+def heatmap_permits(start, end, limit, after=None):
+    log = structlog.get_logger()
+
+    if not bson.objectid.ObjectId.is_valid(after) and after is not None:
+        raise ValueError(f'{after} is not a valid MongoDB ObjectID')
+
+    query = {}
+    if after is not None:
+        query = {
+            '_id': {
+                '$gt': bson.objectid.ObjectId(after),
+            },
+            'issue_date': {
+                '$gt': start,
+                '$lt': end,
+            },
+        }
+    else:
+        query = {
+            'issue_date': {
+                '$gt': start,
+                '$lt': end,
+            },
+        }
+
+    cursor = mongo.db['heatmap'].find(query).limit(limit)
+
+    permits = [permit for permit in cursor]
+
     count = len(permits)
     if count > 0:
         pagination_cursor = str(permits[-1]['_id'])
